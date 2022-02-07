bfdfee67497d0e7c4fa5bc12bd78282b9fb3ad39
Nino Škuflić <32514898+ninoskuflic@users.noreply.github.com>
2022-02-07 20:29:14 +0100

    Updating The JS Bible
    What's new?
    
    - Switch

--- The JS Bible/assets/js/scripts.js
+++ The JS Bible/assets/js/scripts.js
@@ -26,6 +26,7 @@
  * - Cookies
  * - Local storage
  * - JEST Test
+ * - Switch
  *----------------------------------------------------------------------------*/
 
 /*
@@ -454,3 +455,21 @@ test("Adding positive numbers is not a zero", () => {
         }
     }
 });
+
+
+/*
+ * SWITCH -----------------------------------------------------------
+ */
+const expr = 'Papayas';
+switch (expr) {
+    case 'Oranges':
+        console.log('Oranges are $0.59 a pound.');
+        break;
+    case 'Mangoes':
+    case 'Papayas':
+        console.log('Mangoes and papayas are $2.79 a pound.');
+        // expected output: "Mangoes and papayas are $2.79 a pound."
+        break;
+    default:
+        console.log(`Sorry, we are out of ${expr}.`);
+}
